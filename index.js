9c4875a40ccc42da55da2712e11b11f3d6745314
Koichiro Sobue <koichiro.sobue@gmail.com>
2016-02-15 00:03:45 -0800

    remove server

--- index.js
+++ index.js
@@ -2,20 +2,10 @@
 "use strict";
 
 let client = require("./src/client");
-let server = require("./src/server");
-
-const default_Rsrv_port = 6311;
-
-function connect(hostname, port, connectListener) {
-    hostname = hostname !== undefined ? hostname : "localhost";
-    port = port !== undefined ? port : default_Rsrv_port;
-    connectListener = connectListener !== undefined ? connectListener : function(){};
-    
-    return new client(hostname, port, connectListener);
-}
 
 module.exports = {
-    connect: connect,
     Client: client,
-    Server: server
+    connect: function(hostname, port, cb) {
+        return new client(hostname, port, cb);
+    }
 };
