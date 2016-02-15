cbb1ba009de4102267116b1d368ca70064601de0
Koichiro Sobue <koichiro.sobue@gmail.com>
2016-02-15 15:04:53 -0800

    Added ARpt test case

--- test/controlTest.js
+++ test/controlTest.js
@@ -21,7 +21,7 @@ describe("rserve-js's login command", function() {
         });
     });
     
-    it("succeeds with valid credential", function(done) {
+    it("succeeds with valid credential (ARuc)", function(done) {
         let client = Rserve.connect("localhost", 6312, function(loginRequired) {
             expect(loginRequired).to.be.true;
             
@@ -33,9 +33,43 @@ describe("rserve-js's login command", function() {
         });
     });
     
-    it("fails with invalid credential", function(done) {
+    it("fails with invalid credential (ARuc)", function(done) {
         let client = Rserve.connect("localhost", 6312, function(loginRequired) {
             expect(loginRequired).to.be.true;
+            
+            client.login("foo", "buzz", function(err) {
+                expect(err).not.to.be.null;
+                client.close();
+                done();
+            });
+        });
+    });
+    
+    it("succeeds with valid credential (ARpt)", function(done) {
+        let client = Rserve.connect("localhost", 6312, function(loginRequired) {
+            expect(loginRequired).to.be.true;
+            
+            // Hack to force using ARpt.
+            // ARuc seems always enabled and client.js only falls back to ARpt when ARuc is not available.
+            let idx = client.info.indexOf("ARuc");
+            client.info.splice(idx, 1);
+            
+            client.login("foo", "bar", function(err) {
+                expect(err).to.be.null;
+                client.close();
+                done();
+            });
+        });
+    });
+    
+    it("fails with invalid credential (ARpt)", function(done) {
+        let client = Rserve.connect("localhost", 6312, function(loginRequired) {
+            expect(loginRequired).to.be.true;
+            
+            // Hack to force using ARpt.
+            // ARuc seems always enabled and client.js only falls back to ARpt when ARuc is not available.
+            let idx = client.info.indexOf("ARuc");
+            client.info.splice(idx, 1);
             
             client.login("foo", "buzz", function(err) {
                 expect(err).not.to.be.null;
