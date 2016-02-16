db7592ae3cc0ce4b874aae4b473d9a57acce7219
Koichiro Sobue <koichiro.sobue@gmail.com>
2016-02-16 00:52:08 -0800

    More file test

--- test/fileTest.js
+++ test/fileTest.js
@@ -2,6 +2,7 @@
 "use strict";
 
 const spawn = require("child_process").spawn;
+const fs = require("fs");
 const expect = require("chai").expect;
 const Rserve = require("..");
 
@@ -22,14 +23,27 @@ describe("rserve-js", function() {
     });
     
     it("supports writing new file (CMD_createFile, CMD_writeFile, CMD_closeFile)", function(done) {
+        fs.access(__dirname + "/test.txt", fs.R_OK | fs.W_OK, function(err) {
+            expect(err).not.to.be.null; // Make sure file does not yet exist.
+        });
+        
         client.createFile(__dirname + "/test.txt", function(err) {
             expect(err).to.be.null;
+            
+            fs.access(__dirname + "/test.txt", fs.R_OK | fs.W_OK, function(err) {
+                expect(err).to.be.null;
+            });
             
             client.writeFile(new Buffer("written by file test", "utf8"), function(err) {
                 expect(err).to.be.null;
                 
                 client.closeFile(function(err) {
                     expect(err).to.be.null;
+                    
+                    fs.stat(__dirname + "/test.txt", function(err, stat) {
+                        expect(err).to.be.null;
+                        expect(stat.size).to.be.greaterThan(0);
+                    });
                     
                     done();
                 });
@@ -64,8 +78,16 @@ describe("rserve-js", function() {
     });
     
     it("supports removing a file (CMD_removeFile)", function(done) {
+        fs.access(__dirname + "/test.txt", fs.R_OK | fs.W_OK, function(err) {
+            expect(err).to.be.null;
+        });
+        
         client.removeFile(__dirname + "/test.txt", function(err) {
             expect(err).to.be.null;
+            
+            fs.access(__dirname + "/test.txt", fs.R_OK | fs.W_OK, function(err) {
+                expect(err).not.to.be.null; // File no longer exist.
+            });
             
             done();
         });
